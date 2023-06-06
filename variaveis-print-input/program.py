ab4348784cf4ff63af9a357cb62bced5095b7f1a
Samuel Costa <93771098+samuelcostaf@users.noreply.github.com>
2023-06-06 14:23:07 +0200

    Update program.py

---
+++ variaveis-print-input/program.py
@@ -0,0 +1,12 @@
+#cadastro simples utilizando print e input, assim como variaveis basicas.
+print("\nFormulario!")
+nome = str(input("Insira teu nome: "))
+idade = int(input("insira sua idade: "))
+dma = str(input("data de nascimentp(dd-mm-aa): "))
+
+print("\n=======================================================================================")
+
+print("\n Cadastro Completo!\nVerifique por favor se as informações abaico estão corretas: ")
+print("\nNome: ",nome,"\nIdade: ",idade,"\nData de nascimento: ")
+
+
