b95c07950758ce87f9464b4f199c6bfb2be5f272
Samuel Costa <93771098+samuelcostaf@users.noreply.github.com>
2023-06-08 16:27:28 +0200

    Update reconhecimento_fala.py

--- reconhecimento-de-voz/reconhecimento_fala.py
+++ reconhecimento-de-voz/reconhecimento_fala.py
@@ -7,5 +7,5 @@ with sr.Microphone(1) as mic:
     recog.adjust_for_ambient_noise(mic)
     print("Pode me dizer que eu irei transcrever!")
     audio = recog.listen(mic)
-    texto = recog.recognize_google(audio, language="fr")
+    texto = recog.recognize_google(audio, language="pt-BR")
     print(texto)
