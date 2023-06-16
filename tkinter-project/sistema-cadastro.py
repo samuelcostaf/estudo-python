ad1b9a7eebd09b5f89707eb393da016d54993b44
Samuel Costa <93771098+samuelcostaf@users.noreply.github.com>
2023-06-16 21:49:42 +0200

    Update sistema-cadastro.py

--- tkinter-project/sistema-cadastro.py
+++ tkinter-project/sistema-cadastro.py
@@ -27,27 +27,40 @@ class App(ctk.CTk):
 
     def sistema_inteiro(self):
         frame = ctk.CTkFrame(self, width=700, height=50, corner_radius=0, bg_color='orange', fg_color='orange').place(x=0, y=0)
-        title = ctk.CTkLabel(frame, text="Cadastro de Clientes", font=("Century Gothic bold", 24), text_color='#fff')
+        title = ctk.CTkLabel(frame, text="Cadastro de Clientes", font=("Century Gothic bold", 24), text_color='#fff').place(x=190, y=25)
 
         span = ctk.CTkLabel(self, text="Por favor! Preencha todos os campos do formulario", font=("Century Gothic bold", 24), text_color=['#000', '#fff']).place(x=50, y=70)
 
+        def submit():
+            #dados dos entrys
+            name = name_entry.get()
+            contact = contact_value.get()
+            
+            pass
 
+        def clear():
+            pass
+
+        name_value = StringVar
+        contact_value = StringVar
+        endereço_value = StringVar
+        idade_value = StringVar
     #entrys 
 
-        name_entry = ctk.CTkEntry(self, width=350, font=("Century Gohtic", 16), fg_color="transparent")
-        contato_entry = ctk.CTkEntry(self, width=200, font=("Century Gohtic", 16), fg_color="transparent")
-        endereço_entry = ctk.CTkEntry(self, width=200, font=("Century Gohtic", 16), fg_color="transparent")
-        idade_entry = ctk.CTkEntry(self, width=150, font=("Century Gohtic", 16), fg_color="transparent")
+        name_entry = ctk.CTkEntry(self, width=350,textvariable="name_value",font=("Century Gohtic", 16), fg_color="transparent")
+        contato_entry = ctk.CTkEntry(self, width=200, textvariable="contact_value", font=("Century Gohtic", 16), fg_color="transparent")
+        endereço_entry = ctk.CTkEntry(self, width=200, textvariable="endereço_value", font=("Century Gohtic", 16), fg_color="transparent")
+        idade_entry = ctk.CTkEntry(self, width=150, textvariable="idade_value", font=("Century Gohtic", 16), fg_color="transparent")
 
     #Combobox
-        genero_combobox = ctk.CTkComboBox(self, values=["Masculino", "Feminino"], font=("Sans serif", 14))
+        genero_combobox = ctk.CTkComboBox(self, values=["Masculino", "Feminino"], font=("arial", 14))
         genero_combobox.set("Masculino")
 
     #entrada de observações
-        obs_entry = ctk.CTkTextbox(self, width=500, height=150, font=("Arial", 18), border_color="#aaa", border_width=2, fg_color="transparent")
+        obs_entry = ctk.CTkTextbox(self, width=500, height=150, font=("arial", 18), border_color="#aaa", border_width=2, fg_color="transparent")
 
     #criando as labels
-        lb_nome = ctk.CTkLabel(self, text="Nome Completo", font=("Century Gothic bold", 24), text_color=['#000', '#fff'])
+        lb_nome = ctk.CTkLabel(self, text="Nome Completo",  font=("Century Gothic bold", 24), text_color=['#000', '#fff'])
 
         lb_contato = ctk.CTkLabel(self, text="Contato", font=("Century Gothic bold", 24), text_color=['#000', '#fff'])
 
@@ -57,9 +70,10 @@ class App(ctk.CTk):
 
         lb_endereço = ctk.CTkLabel(self, text="Endereço", font=("Century Gothic bold"), text_color=['#000', '#fff'])
 
-        lb_obs = ctk.CTkLabel(self, text="Observações", font=("Century Gothic bold", 24), text_color=['#000', '#fff'])
+        lb_obs = ctk.CTkLabel(self, text="Observações", font=("Century Gothic bold", 16), text_color=['#000', '#fff'])
 
-        btn_submit = ctk.CTkButton(self, text="Salvar Dados".upper(), command=submit, fg_color="#151", hover_color="#131")
+        btn_submit = ctk.CTkButton(self, text="Salvar Dados".upper(), command=submit, fg_color="#151", hover_color="#131").place(x=300,y=420) 
+        btn_submit = ctk.CTkButton(self, text="Limpar Dados".upper(), command=clear, fg_color="#555", hover_color="#333").place(x=500,y=420)
 
     #posicionando os elementos na janela
         lb_nome.place(x=50, y=120)
