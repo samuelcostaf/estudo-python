fe592fd13fda26a4ff8e596657bd9126e65c816a
Samuel Costa <93771098+samuelcostaf@users.noreply.github.com>
2023-06-19 17:08:43 +0200

    Create app.py

---
+++ classes/app.py
@@ -0,0 +1,15 @@
+#criaremos as classes e depois essas classes serão utilizadas
+#orientada a objeto
+#classe = objeto
+
+class Controle:
+    def __init__(self):
+        cor= "black"
+        height = "300"
+        width = "100"
+
+
+controle_remoto = Controle()
+
+
+
